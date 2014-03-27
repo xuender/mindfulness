3c8e43e885ee6233e36e6c296b681ebcb1a32cef
徐晨阳 <xuender@gmail.com>
2014-03-27 11:15:09 +0800

    fix #18

--- web/book/models/chapter.py
+++ web/book/models/chapter.py
@@ -6,6 +6,14 @@ from django.contrib.auth.models import User
 
 from book import Book
 
+class Pages(object):
+    '翻页信息'
+    def __init__(self, first=None, previous=None, next=None, end=None):
+        '初始化'
+        self.first = first
+        self.previous = previous
+        self.next = next
+        self.end = end
 class Chapter(UpdateModel):
     '章节'
     book=models.ForeignKey(Book,
@@ -19,6 +27,24 @@ class Chapter(UpdateModel):
     context=models.TextField(
             verbose_name='正文',
             )
+    ps = None
+    def pages(self):
+        '获取翻页信息'
+        if self.ps == None:
+            cs = self.book.chapters.all()
+            num = 0
+            count = len(cs)
+            for i in range(0, count):
+                if cs[i].id == self.id:
+                    num = i
+            self.ps = Pages()
+            if cs[0].id != self.id:
+                self.ps.first = cs[0]
+                self.ps.previous = cs[num - 1]
+            if cs[count - 1].id != self.id:
+                self.ps.end = cs[count - 1]
+                self.ps.next = cs[num + 1]
+        return self.ps
 
     def anns(self, user):
         return self.annotations.filter(style='a', create_by=user)
